3a1a1a155d43901060f0e4bed4a008462c92c8fb
Rafael-N-Moura <rafaelnm.dev@gmail.com>
2022-01-18 21:28:22 -0300

    Trivia Number Project

---
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:trivia/core/error/failures.dart';
+import 'package:trivia/core/usecases/usecase.dart';
+import 'package:trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class GetRandomNumberTrivia extends UseCase<NumberTrivia, NoParams> {
+  final NumberTriviaRepository repository;
+  GetRandomNumberTrivia(this.repository);
+
+  @override
+  Future<Either<Failure, NumberTrivia>> call(NoParams params) async {
+    return await repository.getRandomNumberTrivia();
+  }
+}
